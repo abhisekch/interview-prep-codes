3736a35f943b27064f5dd9923751c9e0e247cd07
abhisekch <ac1959@cse.jgec.ac.in>
2018-06-24 23:03:19 +0530

    Added solution to problem1 and .gitignore

---
+++ 1a-AddTwoIntegers.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h> //only required for using atoi()
+#include<string.h>
+
+int getNumFromString(const char *str) {
+    int len = strlen(str);
+    int num = 0;
+    if(str[0] == '-') {
+        for(int i = 1; i < len; i++) {
+            num = (num * 10) + (str[i] - 48);
+        }
+        num = 0 - num;
+    }
+    else {
+        for(int i = 0; i < len; i++) {
+            num = (num * 10) + (str[i] - 48);
+        }
+    }
+    return num;
+}
+
+int main(int argc, char const *argv[])
+{
+    if(argc == 1 || argc == 2 || argc > 3) {
+        return 0;
+    }
+    else {
+        int input1 = 0, input2 = 0, result = 0, len = 0;
+        const char *str1, *str2; //const is required to avoid compilation warnings
+        str1 = argv[1];
+        str2 = argv[2];
+        input1 = getNumFromString(str1);
+        input2 = getNumFromString(str2);
+        /*input1 = atoi(argv[1]);
+        input2 = atoi(argv[2]);*/
+        result = input1 + input2;
+        printf("%d\n", result);
+    }
+    return 0;
+}
---
+++ 1b-AddTwoIntegersRecursion.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int addByRecursion(int a, int b) {
+    if(b == 0) {
+        return a;
+    }
+    else if(b > 0) { //when b is a -ve number
+        return addByRecursion(a, b - 1) + 1;
+    }
+    else {
+        return addByRecursion(a, b + 1) - 1;
+    }
+}
+
+int getNumFromString(const char *str) {
+    int len = strlen(str);
+    int num = 0;
+    if(str[0] == '-') {
+        for(int i = 1; i < len; i++) {
+            num = (num * 10) + (str[i] - 48);
+        }
+        num = 0 - num;
+    }
+    else {
+        for(int i = 0; i < len; i++) {
+            num = (num * 10) + (str[i] - 48);
+        }
+    }
+    return num;
+}
+
+int main(int argc, char const *argv[])
+{
+    int input1 = 0, input2 = 0, result = 0, len = 0;
+    const char *str1, *str2; //const is required to avoid compilation warnings
+    str1 = argv[1];
+    str2 = argv[2];
+    input1 = getNumFromString(str1);
+    input2 = getNumFromString(str2);
+    /*input1 = atoi(argv[1]);
+    input2 = atoi(argv[2]);*/
+    result = addByRecursion(input1, input2);
+    printf("%d\n", result);
+    return 0;
+}
