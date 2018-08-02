bf6ab1c7a28be67de5b04550e6bf2c2b512e7113
abhisekch <ac1959@cse.jgec.ac.in>
2018-08-03 01:52:03 +0530

    Removed sorting, user must now input sorted array

--- 44b-BinarySearch.c
+++ 44b-BinarySearch.c
@@ -4,19 +4,6 @@
 #include<ctype.h>
 #include<stdbool.h>
 
-void bubbleSort(int arr[], int size) {
-    int temp;
-    for(int i = 0; i < size - 1; i++) {
-        for(int j = 0; j < size - i -1; j++) {
-            if(arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-}
-
 int binarySearch(int arr[], int searchingElement, int size) {
     int left, right, mid;
     left = 0;
